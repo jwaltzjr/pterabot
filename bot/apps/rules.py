6a812b6551cd8b124bb883e19a7e60b55e67115d
Jeff Waltz Jr <jwaltzjr@krclogistics.com>
2020-02-28 12:53:34 -0600

    Add config for rules role

--- bot/apps/rules.py
+++ bot/apps/rules.py
@@ -1,5 +1,9 @@
 from discord.utils import get
 
+from config import config
+
+RULES_ACCEPTED_ROLE = config['RULES_ACCEPTED_ROLE']
+
 async def refresh_rules(bot, channel_id, rules):
     rules_channel = await bot.fetch_channel(channel_id)
     async for msg in rules_channel.history():
@@ -9,5 +13,5 @@ async def refresh_rules(bot, channel_id, rules):
 
 async def accept_rules(reaction, user, rules_channel):
     if str(reaction.message.channel.id) ==  rules_channel:
-        role = get(reaction.message.guild.roles, name='Member')
+        role = get(reaction.message.guild.roles, RULES_ACCEPTED_ROLE)
         await user.add_roles(role)
