4be2c14923add089880dd31fad7fc7c7f21c5565
Jeff Waltz Jr <jwaltzjr@krclogistics.com>
2020-02-28 12:35:32 -0600

    Add config for command prefix

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,6 +1,13 @@
 from discord.ext.commands import Bot
 
-bot = Bot(command_prefix='!')
+from config import config
+
+try:
+    prefix = config["COMMAND_PREFIX"]
+except KeyError:
+    prefix = '!' # default
+
+bot = Bot(command_prefix=prefix)
 
 from bot.events import *
 from bot.commands import *
